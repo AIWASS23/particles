945fcebd9d3d87cacd0a64e2b33994e650b5ed42
AIWASS23 <marcelodearaujo677@gmail.com>
2025-08-14 08:06:51 -0300

    Initial Commit

---
+++ PocParticles/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  PocParticles
+//
+//  Created by Marcelo deAraújo on 14/08/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ PocParticles/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  PocParticles
+//
+//  Created by Marcelo deAraújo on 14/08/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
